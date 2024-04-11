86453e3f0e8669d9f87baab9e691e4c4be2f6d07
Rodrigoab95 <rodrigo.abarreira@gmail.com>
2024-04-11 16:23:27 -0300

    incluindo rota de navegação

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,35 @@
 // ignore_for_file: prefer_const_constructors
-
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 import 'view/tela_login.dart';
+import 'view/tela_cadastro.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Login',
-      home: TelaLogin(),
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MainApp(),
     ),
   );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login',
+      //
+      // ROTAS DE NAVEGAÇÃO
+      //
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => TelaLogin(),
+        'cadastro': (context) => TelaCadastro(),
+      },
+    );
+  }
 }
